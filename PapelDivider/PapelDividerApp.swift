b0298b7565a4cc1962fba2c97be72a93b80e40c2
yemretekin <emretekin08@gmail.com>
2023-01-29 01:37:35 +0300

    project done.

---
+++ PapelDivider/PapelDividerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PapelDividerApp.swift
+//  PapelDivider
+//
+//  Created by Emre Tekin on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct PapelDividerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
